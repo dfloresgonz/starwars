71cfbe029e96340e7dd52609e731aa88564ad5fb
Diego Rafael Flores Gonzales <dfloresgonz@gmail.com>
2023-08-02 09:57:56 -0500

    cambios9

--- micros/people/configs.js
+++ micros/people/configs.js
@@ -4,8 +4,6 @@ module.exports = async ({ options, resolveConfigurationProperty }) => {
   // Load env vars into Serverless environment
   // You can do more complicated env var resolution with dotenv here
   const envVars = dotenv.config({ path: '.env.local' }).parsed;
-  dotenv.config()
-  console.log(envVars)
   return envVars;
 };
 
